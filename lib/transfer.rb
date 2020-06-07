72004cd7f2a51b97650c7986cbe16ffb1436df19
steve <stevepesce879@gmail.com>
2020-06-07 18:31:03 -0400

    finished Bank_Account and transfer intitializer

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
-class Transfer
-  # your code here
-end
+class Transfer#=====================================
+
+  attr_reader   :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    self.status = "pending"
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+  end
+
+  def valid?
+    return (sender.valid? && receiver.valid?)  
+  end
+
+  
+
+end#===============================================
